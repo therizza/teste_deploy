e9fc957871a34f79ea83df698e31e5eecc6dd2b8
Rizza <matheusrizza@gmail.com>
2024-11-09 13:57:05 -0300

    teste do front

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+//go:embed template/*
+var resources embed.FS
+
+var t = template.Must(template.ParseFS(resources, "template/*"))
 
 func main() {
-	fmt.Println("Hello, World! XD")
+	port := "8080"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]string{
+			"Hello": "Hello World!!",
+		}
+
+		t.ExecuteTemplate(w, "index.html.tmpl", data)
+	})
+
+	log.Println("Server started on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
