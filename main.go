b3738cdf3faa59bf90965f3324f04eaf0d3a2042
Rizza <matheusrizza@gmail.com>
2024-11-10 08:23:11 -0300

    adição de novas telas

--- main.go
+++ main.go
@@ -12,14 +12,23 @@ var resources embed.FS
 
 var t = template.Must(template.ParseFS(resources, "template/*"))
 
+type Items struct {
+	Nome string
+	Id   int
+}
+
 func main() {
 	port := "8080"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := map[string]string{
-			"Hello": "Hello World!!",
+		data := map[string]interface{}{
+			"Titulo": "Tião",
+			"Items": []Items{
+				{Nome: "Teste-deploy-backend", Id: 1},
+				{Nome: "Teste-deploy-frontend", Id: 2},
+			},
 		}
 
-		t.ExecuteTemplate(w, "index.html.tmpl", data)
+		t.ExecuteTemplate(w, "index.html", data)
 	})
 
 	log.Println("Server started on port", port)
